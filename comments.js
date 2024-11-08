9b5822a237250b44a974168e14e6ed071327f571
neamima <126111002+neamima@users.noreply.github.com>
2024-11-08 20:49:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,13 @@
-//create web server
+//Create web server
+var express = require('express');
+var app = express();
+
+//Create a route for the path /comments
+app.get('/comments', function(req, res) {
+  res.send('Comments will be here!');
+});
+
+//Bind server to port 8080
+app.listen(8080, function() {
+  console.log('Listening on port 8080...');
+});
